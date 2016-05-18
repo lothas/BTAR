aa86ad11da9cb0efe254ad838bee36d4d33d1bd2
AlonShoshan <alonshoshan10@gmail.com>
2016-05-18 17:58:30 +0300

    Bravo7.2 update in openGL
    showing a 3D cube ass object

---
+++ sandbox/Bravo7.2_openGL_update/src/bravo/game/manager/GameManager.java
@@ -0,0 +1,94 @@
+package bravo.game.manager;
+
+import com.MobileAnarchy.Android.Widgets.Joystick.JoystickView;
+import com.example.bravo.MainActivity;
+import com.example.bravo.R;
+
+import android.util.Log;
+import android.widget.TextView;
+
+public class GameManager {
+	private MainActivity mMainActivity;
+	private TextView mTextDebug; //TopLeft
+	private TextView mTextBravoVersion;
+	private String mText;
+	private boolean flag;
+	
+	private static final String TAG = "gameMen";
+	
+	public GameManager(MainActivity activity) {
+		mMainActivity = activity;
+		mTextDebug = (TextView) mMainActivity.findViewById(R.id.TextDebug);
+		mTextDebug.setText(String.format("DebugText"));
+		mTextBravoVersion = (TextView) mMainActivity.findViewById(R.id.TextBravoVersion);
+		mTextBravoVersion.setText(String.format("Bravo7.1"));
+		flag = true;
+	}
+	
+	public void printTopLeft(String text){
+		mText = text;
+		mMainActivity.runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                // This code will always run on the UI thread, therefore is safe to modify UI elements.
+            	mTextDebug.setText(String.format(mText));
+            }
+        });
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*try {
+if (flag == true){
+	flag = false;
+	mTextDebug.setText(String.format("try"));
+}
+} catch (Exception e) {
+Log.d(TAG, "catch :" + e.getMessage());
+}*/
