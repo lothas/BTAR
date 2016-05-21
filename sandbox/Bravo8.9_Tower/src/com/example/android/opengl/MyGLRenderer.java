d2aa3a0bb2bcee6410967396f3184f02f87cbfd4
AlonShoshan <alonshoshan10@gmail.com>
2016-05-21 22:03:27 +0300

    bravo8.9 first towers

---
+++ sandbox/Bravo8.9_Tower/src/com/example/android/opengl/MyGLRenderer.java
@@ -0,0 +1,331 @@
+/*
+ * Copyright (C) 2011 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.example.android.opengl;
+
+import java.sql.NClob;
+
+import javax.microedition.khronos.egl.EGLConfig;
+import javax.microedition.khronos.opengles.GL10;
+
+import org.opencv.samples.colorblobdetect.MarkerDetector;
+
+import com.example.bravo.MainActivity;
+
+import android.content.Context;
+import android.graphics.Point;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.opengl.GLSurfaceView;
+import android.opengl.GLU;
+import android.util.Log;
+import android.view.Display;
+import bravo.game.manager.GameManager;
+
+/**
+ * Provides drawing instructions for a GLSurfaceView object. This class
+ * must override the OpenGL ES drawing lifecycle methods:
+ * <ul>
+ *   <li>{@link android.opengl.GLSurfaceView.Renderer#onSurfaceCreated}</li>
+ *   <li>{@link android.opengl.GLSurfaceView.Renderer#onDrawFrame}</li>
+ *   <li>{@link android.opengl.GLSurfaceView.Renderer#onSurfaceChanged}</li>
+ * </ul>
+ */
+public class MyGLRenderer implements GLSurfaceView.Renderer {
+	private static final String  TAG              = "MyGLRenderer";
+	//-------------------------Static----------------------------//
+    private static final int     NO_COLOR = -1;
+	private static final int     BLUE_COLOR = 1;
+	private static final int     GREEN_COLOR = 2;
+	private static final int     RED_COLOR = 3;
+	//-------------------------Fields--------------------------//
+    private Triangle mTriangle;
+    private Square mSquare;
+    private example mExample;
+    private BlanderObj mBlanderObj;
+    private AssetObj mTower;
+    private AssetObj mCoin;
+    private AssetObj mTest;
+    private float mAngle;
+    
+    private float objX;
+    private float objY;
+    
+    private int screenWidth;
+    private int screenHeight;
+    
+    private  MarkerDetector blueRedDetector;
+    private  MarkerDetector GreenRedDetector;
+    private  MarkerDetector GreenBlueDetector;
+    
+    SensorManager sensorManager;
+	int orientationSensor;
+	float headingAngle;
+	float pitchAngle;
+	float rollAngle;
+	
+	//-------------------------GameManger----------------------//
+	public GameManager mGameManager;
+    //-------------------------Activity------------------------//
+  	MainActivity                 mMainActivity;	
+  	private Context              appContext;
+  	//-------------------------Methods-------------------------//
+    
+  	public MyGLRenderer(Context context){
+  		appContext = context;
+  		
+  	}
+  	
+    @Override
+    public void onSurfaceCreated(GL10 gl, EGLConfig config) {
+        // Set the background frame color
+        gl.glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+        
+        load3dObjects();       
+        runOrientationSensor();
+        mGameManager = mMainActivity.mGameManager;
+        getScreenSizeInPixels(); // sets screenWidth and screenHeight
+       
+        objX = -0.5f; 
+        objY = 0.8f;
+        
+        blueRedDetector = new MarkerDetector();
+        blueRedDetector.prepareGame(screenWidth, screenHeight, BLUE_COLOR, RED_COLOR, 4);
+        GreenRedDetector = new MarkerDetector();
+        GreenRedDetector.prepareGame(screenWidth, screenHeight, GREEN_COLOR, BLUE_COLOR, 4);
+        GreenBlueDetector = new MarkerDetector();
+        GreenBlueDetector.prepareGame(screenWidth, screenHeight, GREEN_COLOR, BLUE_COLOR, 4);
+        
+        int robpId = com.example.bravo.R.drawable.robo;
+        mGameManager.printBottomRight("screenWidth: " + screenWidth + " screenHeight: " + screenHeight);
+        mBlanderObj.createTexture(gl, appContext, robpId);
+        
+    }
+    
+    private void load3dObjects(){
+    	mTriangle = new Triangle();
+        mSquare = new Square();
+        mExample = new example();
+        mBlanderObj = new BlanderObj();
+        mTower = new AssetObj(mMainActivity, "tower2.obj","tower2.mtl");
+        mCoin = new AssetObj(mMainActivity, "t2.obj","object2.mtl");
+        mTest = new AssetObj(mMainActivity, "t2.obj","object2.mtl");       
+    }
+    
+    private void getScreenSizeInPixels(){
+    	Display display = mMainActivity.getWindowManager().getDefaultDisplay();
+        Point size = new Point();
+        display.getSize(size);
+        screenWidth = size.x;
+        screenHeight = size.y;
+    }
+
+    @Override
+    public void onDrawFrame(GL10 gl) {
+    	// Clears the screen and depth buffer.
+    	gl.glClearColor(0,0,0,0);
+    	gl.glClear(GL10.GL_COLOR_BUFFER_BIT | GL10.GL_DEPTH_BUFFER_BIT);
+    	// Replace the current matrix with the identity matrix
+    	gl.glViewport(0, 0, screenWidth, screenWidth);
+    	gl.glLoadIdentity();
+    	
+    	// SQUARE A
+    	drawObj(gl, blueRedDetector);
+    	drawObj(gl, GreenRedDetector);
+    	drawObj(gl, GreenBlueDetector);
+    	drawCoin(gl);
+    	// DRAW GTID
+    	//drawGrid(gl,1,1,false);
+    	//drawGrid(gl,1,-1,false);
+    	//drawGrid(gl,0,1,false);
+    	//drawGrid(gl,1,0,false);
+    	//drawGrid(gl,0,0,true);
+    	//drawGrid(gl,-1,0,false);
+    	//drawGrid(gl,0,-1,false);
+    	//drawGrid(gl,-1,1,false);
+    	//drawGrid(gl,-1,-1,false);
+    }
+
+    
+    public void onSurfaceChanged(GL10 gl, int width, int height) {
+        // Adjust the viewport based on geometry changes
+        // such as screen rotations
+        gl.glViewport(0, 0, width, height);
+
+        // make adjustments for screen ratio
+        float ratio = (float) width / height;
+        gl.glMatrixMode(GL10.GL_PROJECTION);        // set matrix to projection mode
+        gl.glLoadIdentity();                        // reset the matrix to its default state
+        gl.glFrustumf(-ratio, ratio, -1, 1, 3, 7);  // apply the projection matrix
+    
+    }
+
+    /**
+     * Returns the rotation angle of the triangle shape (mTriangle).
+     *
+     * @return - A float representing the rotation angle.
+     */
+    public float getAngle() {
+        return mAngle;
+    }
+
+    /**
+     * Sets the rotation angle of the triangle shape (mTriangle).
+     */
+    public void setAngle(float angle) {
+        mAngle = angle;
+    }
+    
+    public void setScreenWidthHeight(int width, int height){
+    	screenWidth = width; 
+    	screenHeight = height;
+    }
+    
+    public void setCordinates(float x, float y){
+   	 	objX = x; 
+        objY = y;
+   }
+    
+    public float pixel2worldX(int x){
+    	return 2*((float)x) / screenWidth - 1;
+    }
+    
+    public float pixel2worldY(float y){
+    	// we want ower world to be scaled 1:1 so points 1 and -1 of the Y axis are out side the screen
+    	//that is why we use (screenHeight/ screenWidth) insted of 1
+    	float hightWidth = ((float)screenHeight / (float)screenWidth); // the (float) are importent.
+    	return y * hightWidth - (1f - hightWidth);  
+    }
+    
+    /*public float pixel2worldYscaled(int y){
+    	return 1 - 2*((float)y) / screenHeight;
+    }*/
+    
+    public float pixel2worldYscaled(int y){// need to add later
+    	float tYemp = 1 - 2*((float)y) / screenHeight;
+    	return pixel2worldY(tYemp);
+    }
+    
+    public float scaleTrackObjects(MarkerDetector mDetector){
+    	Log.i(TAG, "MyGLRenderer: mDetector xF - xB = " + (mDetector.getFrontX() - mDetector.getBackX()));
+    	Log.i(TAG, "MyGLRenderer: mDetector yF - yB = " + (mDetector.getFrontY() - mDetector.getBackY()));
+    	Log.i(TAG, "MyGLRenderer: mDetector.getFront2BackLength() = " + mDetector.getFront2BackLength());
+    	return 5*(mDetector.getFront2BackLength()) / mDetector.getFrameWidth();
+    }
+    
+    public void setTrackObjects(MarkerDetector mDetector1,MarkerDetector mDetector2,MarkerDetector mDetector3){
+    	blueRedDetector = mDetector1;
+    	GreenRedDetector = mDetector2;
+    	GreenBlueDetector = mDetector3;
+    }
+    
+    public void setMainActivityContext(MainActivity activity){
+    	mMainActivity = activity;
+    }
+    
+    public void runOrientationSensor(){
+    	sensorManager = (SensorManager) mMainActivity.getSystemService(mMainActivity.SENSOR_SERVICE);
+		orientationSensor = Sensor.TYPE_ORIENTATION;
+		sensorManager.registerListener(sensorEventListener,sensorManager.getDefaultSensor(orientationSensor),SensorManager.SENSOR_DELAY_NORMAL);
+    }
+    
+    final SensorEventListener sensorEventListener = new SensorEventListener() {
+		public void onSensorChanged(SensorEvent sensorEvent) {
+			if (sensorEvent.sensor.getType() == Sensor.TYPE_ORIENTATION) {
+				headingAngle = sensorEvent.values[0];
+				pitchAngle = sensorEvent.values[1];
+				rollAngle = sensorEvent.values[2];
+				Log.d(TAG, "Heading: " + String.valueOf(headingAngle));
+				Log.d(TAG, "Pitch: " + String.valueOf(pitchAngle));
+				Log.d(TAG, "Roll: " + String.valueOf(rollAngle));
+			}
+		}
+
+		public void onAccuracyChanged(Sensor senor, int accuracy) {
+			// Not used
+		}
+	};
+    
+    public void drawObj(GL10 gl, MarkerDetector mDetector){
+		if (mDetector.isDetected() == false) {
+			return;
+		}
+		// Save the current matrix.
+		gl.glPushMatrix();
+		gl.glTranslatef(pixel2worldX(mDetector.getMiddleX()), pixel2worldYscaled(mDetector.getMiddleY()), 0);
+		float scale = 0.4f * scaleTrackObjects(mDetector);
+		gl.glScalef(scale, scale, scale); //(screenHeight/screenWidth)*		
+		float rotateOnZ = mDetector.getAngleFromXAxis();
+		gl.glRotatef(rotateOnZ, 0, 0, 1);
+		gl.glRotatef(rollAngle, (float)Math.cos(Math.toRadians(rotateOnZ)), -(float)Math.sin(Math.toRadians(rotateOnZ)), 0);
+				
+		// Draw mExample A.
+		mTower.draw(gl);
+		// Restore the last matrix.
+		gl.glPopMatrix();
+	}
+    
+    public void drawCoin(GL10 gl){
+    	if (blueRedDetector.hasCoin() == true){
+    		drawCoinToWorld(gl, blueRedDetector);
+    	}
+    	if (GreenRedDetector.hasCoin() == true){
+    		drawCoinToWorld(gl, GreenRedDetector);
+    	}
+    	if (GreenBlueDetector.hasCoin() == true){
+    		drawCoinToWorld(gl, GreenBlueDetector);
+    	}
+    }
+    
+    public void drawCoinToWorld(GL10 gl, MarkerDetector mDetector){
+    	gl.glPushMatrix();
+		gl.glTranslatef(pixel2worldX(mDetector.getCoinX()), pixel2worldYscaled(mDetector.getCoinY()), 0);
+		float scale = 0.2f * scaleTrackObjects(mDetector);
+		gl.glScalef(scale, scale, scale); 	
+		float rotateOnZ = mDetector.getAngleFromXAxis(); //TODO rotate according to time;
+		gl.glRotatef(rotateOnZ, 0, 0, 1);
+		gl.glRotatef(rollAngle, (float)Math.cos(Math.toRadians(rotateOnZ)), -(float)Math.sin(Math.toRadians(rotateOnZ)), 0);
+				
+		// Draw mExample A.
+		mCoin.draw(gl);
+		// Restore the last matrix.
+		gl.glPopMatrix();
+    }
+    
+    public void drawGrid(GL10 gl,float x, float y, boolean flag){
+		// Save the current matrix.
+		gl.glPushMatrix();
+		//gl.glTranslatef(pixel2worldX(mDetector.getMiddleX()), pixel2worldY(mDetector.getMiddleY()), 0);
+		gl.glTranslatef(x, pixel2worldY(y), 0);
+		
+		float scale = 0.5f;
+		Log.i(TAG, "MyGLRenderer: sacle = " + scale);
+		gl.glScalef(scale, scale, scale); 
+		//gl.glRotatef(45,0,0,1);				
+		// Draw mExample A.
+		//mExample.draw(gl);
+		if (flag){
+			mTriangle.draw(gl);
+		}
+		mTower.draw(gl);
+		// Restore the last matrix.
+		gl.glPopMatrix();
+	}
+    
+    
+    
+}
---
+++ sandbox/Bravo8.9_Tower/src/com/example/android/opengl/myParser.java
@@ -0,0 +1,330 @@
+package com.example.android.opengl;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.nio.FloatBuffer;
+import java.util.Iterator;
+import java.util.Scanner;
+import java.util.Vector;
+
+import android.util.Log;
+
+/** Assumes UTF-8 encoding. JDK 7+. */
+public class myParser {
+	
+	private final int NUM_XYZ_ELS;
+	private final int NUM_NXYZ_ELS;
+	private final int NUM_RGBA_ELS;
+	private final int NUM_ST_ELS;
+	public Vector<Vector<Float>> v = new Vector<Vector<Float>>();
+	private Vector<Integer> vOrder = new Vector<Integer>();
+	private Vector<Vector<Float>> vn = new Vector<Vector<Float>>();
+	private Vector<Integer> vnOrder = new Vector<Integer>();
+	private Vector<Vector<Float>> tempC = new Vector<Vector<Float>>();
+	private Vector<Vector<Float>> c = new Vector<Vector<Float>>();
+	private Vector<Vector<Float>> vt = new Vector<Vector<Float>>();
+	private Vector<Integer> vtOrder = new Vector<Integer>();
+	public Vector<Vector<Float>> interleavedVec = new Vector<Vector<Float>>();
+	public float[] vertices;
+	public byte[] indices;
+	public byte[] colors;
+	public float[] normals;
+	public byte[] normIndices;
+	public float[] texture;
+	public byte[] texIndices;
+	public float[] interleavedArray;
+	private final InputStream objFile;
+	private final InputStream mtlFile;
+	private int m_NumVertices = 0;
+    private final int size;
+    private FloatBuffer mFVertexBuffer;
+    private ByteBuffer mColorBuffer;
+    private ByteBuffer mIndexBuffer;
+    private int vertexNum = 0;
+    private int objNum = 0;
+    private Boolean objFlag = false;
+    private Boolean objFlagPrev = false;
+
+	public myParser(InputStream aObjFile, InputStream aMtlFile, int vert, int norm, int color, int tex){
+		objFile = aObjFile;
+		mtlFile = aMtlFile;
+		NUM_XYZ_ELS = vert;
+		NUM_NXYZ_ELS = norm;
+		NUM_RGBA_ELS = color;
+		NUM_ST_ELS = tex;
+		size = NUM_XYZ_ELS+NUM_NXYZ_ELS+NUM_RGBA_ELS+NUM_ST_ELS;
+	}
+
+	public void run(){
+		processLineByLine();
+		makeCombVec();
+		translateVec();
+		createBuffer();
+		m_NumVertices = indices.length;
+	}
+	
+	public final void processLineByLine(){
+		Scanner scanner =  new Scanner(mtlFile);
+		while (scanner.hasNextLine()){
+			processMtl(scanner.nextLine());
+		}
+		scanner.close();
+		scanner =  new Scanner(objFile);
+		while (scanner.hasNextLine()){
+			processLine(scanner.nextLine());
+		}
+		if(objFlagPrev == true){
+			spreadColor(vertexNum);
+		}
+		scanner.close();
+		
+	}
+	
+
+	protected void processLine(String aLine){
+    //use a second Scanner to parse the content of each line 
+	  	Scanner scanner = new Scanner(aLine);
+	    if (scanner.hasNext()){
+		      //assumes the line has a certain structure
+		    String name = scanner.next();
+	    	objFlagPrev = objFlag;
+	    	if (name.equals("f")) {
+	    		objFlag = true;
+	    		addVrtice(scanner.next());
+	    		addVrtice(scanner.next());
+	    		addVrtice(scanner.next());
+	    	}else{
+	    		objFlag = false;
+	    		if(objFlagPrev == true){
+	    			spreadColor(vertexNum);
+	    			vertexNum = 0;
+	    			objNum++;
+	    		}
+	    	}
+		    if(name.equals("v")) {
+	        	v.add(floatVec(scanner));
+	        	vertexNum++;
+		    }else if (name.equals("vn")) {
+	        	vn.add(floatVec(scanner));
+			}else if (name.equals("vt")) {
+	        	vt.add(floatVec(scanner));
+			}
+	    }	 
+	}
+	
+	protected void processMtl(String aLine){
+		  	Scanner scanner = new Scanner(aLine);
+		    if (scanner.hasNext()){
+			    String name = scanner.next();
+		    	if (name.equals("Kd")) {
+		    		Vector<Float> temp = new Vector<Float>();
+		    		while(scanner.hasNextFloat()){
+		    			temp.add(255*scanner.nextFloat());		    			
+		    		}
+		    		tempC.add(temp);
+		    	}
+		    }
+		    scanner.close();
+		}
+	
+	public void spreadColor(int amount){
+		Vector<Float> temp = new Vector<Float>();
+		temp = tempC.elementAt(objNum);
+		temp.add(255f);
+		for (int i=0; i< amount; i++){
+			c.add(temp);
+		}
+	}
+	
+	public void addVrtice(String values){
+		Scanner scanner = new Scanner(values);
+		scanner.useDelimiter("/");
+		int j=0;
+		while (scanner.hasNext()) {
+			try{
+				int foo = Integer.parseInt(scanner.next())-1;
+				int i=0;
+				switch(j) {
+			        case 0: vOrder.add(foo);
+			        	break;
+			        case 1: vtOrder.add(foo);
+			        	break;
+			        case 2: vnOrder.add(foo);
+			        	break;
+			        default: ;
+			        	break;
+				    }
+			}catch(Exception e){ 
+        		//Log.v("hi", e.getMessage());
+			}
+			j++;
+		}
+    	scanner.close();
+	}
+	
+	public void makeCombVec(){
+		Iterator<Integer> vIt = vOrder.iterator();
+		Iterator<Integer> vnIt = vnOrder.iterator();
+		Iterator<Integer> vtIt = vtOrder.iterator();
+		Vector<Float> temp = new Vector<Float>();
+		int i=0;
+		while(vIt.hasNext()){
+			int vInd = vIt.next();
+			temp.addAll(v.elementAt(vInd));
+			if(NUM_NXYZ_ELS > 0){
+				i++;
+				int vnInd = vnIt.next();
+				temp.addAll(vn.elementAt(vnInd));
+			}
+			if(NUM_RGBA_ELS > 0){
+				temp.addAll(c.elementAt(vInd));
+			}
+			if(NUM_ST_ELS > 0){
+				int vtInd = vtIt.next();
+				temp.addAll(vt.elementAt(vtInd));
+			}
+		}
+		interleavedVec.add(temp);
+	}
+	
+	
+	public Vector<Float> floatVec(Scanner scanner){
+		Vector<Float> temp = new Vector<Float>();
+		float x = scanner.nextFloat();
+		float z = scanner.nextFloat();
+		float y = scanner.nextFloat();
+		temp.add(x);
+		temp.add(y);
+		temp.add(-z);
+		return temp;
+	}
+	
+	
+	public void translateVec(){
+		vertices = floatVecToFloatArray(v);
+		if(NUM_RGBA_ELS == 0){
+			generateDefaultColor();
+		} else {			
+			colors = floatVecToByteArray(c);
+		}
+		if(NUM_NXYZ_ELS > 0){
+			normals = floatVecToFloatArray(vn);
+			normIndices = byteVecToArray(vnOrder);
+		}
+		if(NUM_ST_ELS > 0){
+			texture = floatVecToFloatArray(vt);
+			texIndices = byteVecToArray(vtOrder);
+		}
+		indices = byteVecToArray(vOrder);
+		interleavedArray = floatVecToFloatArray(interleavedVec);
+		
+	}
+	
+	public void createBuffer(){
+		ByteBuffer vbb = ByteBuffer.allocateDirect(vertices.length * 4); //4 bytes per float
+		vbb.order(ByteOrder.nativeOrder());
+		
+		mFVertexBuffer = vbb.asFloatBuffer();
+		mFVertexBuffer.put(vertices);
+		mFVertexBuffer.position(0);
+
+		mColorBuffer = ByteBuffer.allocateDirect(colors.length);
+		mColorBuffer.put(colors);
+		mColorBuffer.position(0);
+
+		mIndexBuffer = ByteBuffer.allocateDirect(indices.length);
+		mIndexBuffer.put(indices);
+		mIndexBuffer.position(0);
+		
+		/*if(NUM_NXYZ_ELS > 0){
+			normals = floatVecToFloatArray(vn);
+			normIndices = byteVecToArray(vnOrder);
+		}
+		if(NUM_ST_ELS > 0){
+			texture = floatVecToFloatArray(vt);
+			texIndices = byteVecToArray(vtOrder);
+		}*/
+
+	}
+	
+	public void generateDefaultColor(){
+		colors = new byte[v.size()*4]; 
+		//for(int i =0; i < colors.length; i++){
+		//	colors[i] = (byte) 255;			
+		//}
+		for(int i =0; i < colors.length; i = i + 4){
+			colors[i] =     (byte) ((i*50)%255);
+			colors[i + 1] = (byte) ((i*100)%255);	
+			colors[i + 2] = (byte) ((i*200)%255);	
+			colors[i + 3] = (byte) 255;	
+		}
+	}
+	
+	public float[] floatVecToFloatArray(Vector<Vector<Float>> tempVec){
+		Iterator<Vector<Float>> it = tempVec.iterator();
+		float[] tempArr= new float[tempVec.size()*tempVec.firstElement().size()]; 
+		int i = 0;
+		while(it.hasNext()){
+			Iterator<Float> init = it.next().iterator();
+			while(init.hasNext()){
+				tempArr[i] = init.next();
+				i++;
+			}
+		}
+		return tempArr;
+	}
+	
+	public byte[] floatVecToByteArray(Vector<Vector<Float>> tempVec){
+		Iterator<Vector<Float>> it = tempVec.iterator();
+		byte tempArr[] = new byte[tempVec.size()*tempVec.firstElement().size()]; 
+		int i = 0;
+		while(it.hasNext()){
+			Iterator<Float> init = it.next().iterator();
+			while(init.hasNext()){
+				tempArr[i] = init.next().byteValue();;
+				i++;
+			}
+		}
+		return tempArr;
+	}
+	
+	public byte[] byteVecToArray(Vector<Integer> tempVec){
+		Iterator<Integer> it = tempVec.iterator();
+		byte[] tempArr= new byte[tempVec.size()]; 
+		int i = 0;
+		while(it.hasNext()){
+			tempArr[i] =  it.next().byteValue();
+			i++;
+		}
+		return tempArr;
+	}
+
+	public int getM_NumVertices() {
+		return m_NumVertices;
+	}
+
+	public FloatBuffer getFVertexBuffer() {
+		return mFVertexBuffer;
+	}
+
+	public ByteBuffer getColorBuffer() {
+		return mColorBuffer;
+	}
+
+	public ByteBuffer getIndexBuffer() {
+		return mIndexBuffer;
+	}
+	/*protected void findVertices(String aLine){
+		Scanner scanner = new Scanner(aLine);
+	    if (scanner.hasNext()){
+	    	 String name = scanner.next();
+	    	 if (name.equals("f")){
+	    		 m_NumVertices++;
+	    	 }
+	    }
+	    scanner.close();
+	}*/
+	
+
+} 
