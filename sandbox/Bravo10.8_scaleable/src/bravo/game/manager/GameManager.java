d136c65b9b9206abe7d772008d92beab4b3389c3
alonamar <alonamar2@gmail.com>
2016-07-28 15:19:02 +0300

    Bravo10.8_scaleable

---
+++ sandbox/Bravo10.8_scaleable/src/bravo/game/manager/GameManager.java
@@ -0,0 +1,289 @@
+package bravo.game.manager;
+
+import java.util.Random;
+
+import org.opencv.samples.colorblobdetect.MarkerDetector;
+
+import com.MobileAnarchy.Android.Widgets.Joystick.JoystickView;
+import com.example.bravo.MainActivity;
+import com.example.bravo.R;
+
+import android.util.Log;
+import android.widget.TextView;
+
+public class GameManager {
+	public static final int     NUM_OF_TOWERS = 6;
+	private MainActivity mMainActivity;
+	private TextView mTextDebug; //TopLeft
+	private TextView mTextTimeStemp; //TopRight
+	private TextView mTextBottomRight; //BottomRight
+	private TextView mTextBravoVersion;
+	private String mText;
+	private int mCoinDistribution, mNumOfTowers, mLazerDistribution;
+	private Random mRandomGenerator;
+	private boolean flagForDebug, gameStarted;
+	enum direction {keepGO, stop, front, back};
+	
+	private GameTimer mGameTimer;
+	
+	private static final String TAG = "gameMen";
+	
+	public GameManager(MainActivity activity) {
+		gameStarted = false;
+		mNumOfTowers = NUM_OF_TOWERS;
+		mMainActivity = activity;
+		mTextDebug = (TextView) mMainActivity.findViewById(R.id.TextDebug);
+		mTextDebug.setText(String.format("DebugText"));
+		mTextBravoVersion = (TextView) mMainActivity.findViewById(R.id.TextBravoVersion);
+		mTextBravoVersion.setText(String.format("Bravo10.4_after_exams"));
+		mTextTimeStemp = (TextView) mMainActivity.findViewById(R.id.TextTimeStemp);
+		mTextTimeStemp.setText(String.format("TimeStempText"));
+		mTextBottomRight = (TextView) mMainActivity.findViewById(R.id.TextBottomRight);
+		mTextBottomRight.setText(String.format("BR"));
+		
+		mGameTimer = new GameTimer(mMainActivity);
+		mGameTimer.start();
+		
+		mRandomGenerator = new Random();
+		
+		
+		flagForDebug = true;
+	}
+	
+	public void printTopRight(String text){
+		mText = text;
+		mMainActivity.runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                // This code will always run on the UI thread, therefore is safe to modify UI elements.
+            	mTextDebug.setText(String.format(mText));
+            }
+        });
+	}
+	
+	public void printTopLeft(String text){
+		mText = text;
+		mMainActivity.runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                // This code will always run on the UI thread, therefore is safe to modify UI elements.
+            	mTextTimeStemp.setText(String.format(mText));
+            }
+        });
+	}
+	
+	public void printBottomRight(String text){
+		mText = text;
+		mMainActivity.runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                // This code will always run on the UI thread, therefore is safe to modify UI elements.
+            	mTextBottomRight.setText(String.format(mText));
+            }
+        });
+	}
+	
+	public int CoinDistribution(boolean genarateNewCoin){
+		if (genarateNewCoin == true){
+			mCoinDistribution = mRandomGenerator.nextInt(mNumOfTowers);
+			mMainActivity.tracking.setCoinDistribution(mCoinDistribution); 
+		}
+		return mCoinDistribution;
+	}
+	
+	public boolean GameIsRuning(){
+		return gameStarted;
+	}
+	
+	public void GameStarted(){
+		gameStarted = true;		
+		CoinDistribution(true); //Generating new coin
+	}
+	
+	public void rotateCoin(){
+		for(int i = 0; i < mNumOfTowers; i++){
+			if (mMainActivity.tracking.mDetectorArry[i].hasCoin() == true){
+				mMainActivity.tracking.mDetectorArry[i].updateCoinAngle();
+				break;
+			}
+		}
+	}
+	
+	public void objCheck(){
+		direction js = direction.keepGO;
+		if(mMainActivity.tracking.myRobot.isDetected()){
+			js = objHitCheck();
+			coinCollect();
+		}else{
+			js = robNotFound();				
+		}
+		sendJS(js);
+	}
+	
+	public direction objHitCheck(){
+		float sizeFactor = 4;
+		float robSize = mMainActivity.tracking.myRobot.getFront2BackLength();
+		for(int i = 0; i < mNumOfTowers; i++){
+			if(!mMainActivity.tracking.mDetectorArry[i].isDetected()){
+				continue;
+			}
+			float objCenterX = mMainActivity.tracking.mDetectorArry[i].getMiddleX();
+			float objCenterY = mMainActivity.tracking.mDetectorArry[i].getMiddleY();
+			float centersDist = getDist(mMainActivity.tracking.myRobot.getMiddleX(), 
+					mMainActivity.tracking.myRobot.getMiddleY(),
+					objCenterX, objCenterY);
+			float sizeDistRatio = centersDist/robSize;
+			if ( sizeDistRatio < sizeFactor ){ //TODO: find optimal
+				//stop robot and let it drive only one way
+				
+				float distBack = getDist(mMainActivity.tracking.myRobot.getBackX(), 
+						mMainActivity.tracking.myRobot.getBackY(),
+						objCenterX, objCenterY);
+				float distFront = getDist(mMainActivity.tracking.myRobot.getFrontX(), 
+						mMainActivity.tracking.myRobot.getFrontY(),
+						objCenterX, objCenterY);
+				if(distBack < distFront){
+					//send only front
+					return direction.front;
+				} else {
+					//send only back
+					return direction.back;
+				}
+			}
+		}
+		return direction.keepGO;
+	}
+	
+	public void coinCollect(){
+		float sizeFactor = 1f;
+		float robSize = mMainActivity.tracking.myRobot.getFront2BackLength();
+		for(int i = 0; i < mNumOfTowers; i++){
+			if(!mMainActivity.tracking.mDetectorArry[i].isDetected()){
+				continue;
+			}
+			float objCenterX = mMainActivity.tracking.mDetectorArry[i].getCoinX();
+			float objCenterY = mMainActivity.tracking.mDetectorArry[i].getCoinY();
+			float centersDist = getDist(mMainActivity.tracking.myRobot.getMiddleX(), 
+					mMainActivity.tracking.myRobot.getMiddleY(),
+					objCenterX, objCenterY);
+			float sizeDistRatio = centersDist/robSize;
+			if ( sizeDistRatio < sizeFactor ){ //TODO: find optimal
+				CoinDistribution(true);
+			}
+		}
+	}
+	
+	public void sendJS(direction js){
+		double jsX = mMainActivity.joystick.getX();
+		double jsY = mMainActivity.joystick.getY();
+		if(js == direction.back){
+			mMainActivity.joystick.haltDrive(true);
+			if(jsX < 45 && jsX > -45 && jsY < 0){
+				mMainActivity.joystick.haltDrive(false);
+			}
+		}else if(js == direction.front){
+			mMainActivity.joystick.haltDrive(true);
+			if(jsX < 45 && jsX > -45 && jsY > 0){
+				mMainActivity.joystick.haltDrive(false);
+			}
+		}else if(js == direction.stop){
+			mMainActivity.joystick.haltDrive(true);
+		}else{
+			mMainActivity.joystick.haltDrive(false);
+		}
+	}
+	
+	public direction robNotFound(){
+		//send robot not to move
+		return direction.stop;
+	}
+	
+	public float getDist(float fX, float fY, float bX, float bY) {
+		return (float)Math.sqrt((double)((fX - bX)*(fX - bX) + (fY - bY)*(fY - bY)));
+	}
+	
+	public int laserDistribution(boolean genarateLazer){
+		if (genarateLazer == true){
+			mLazerDistribution = mRandomGenerator.nextInt(mNumOfTowers);
+		}
+		return mLazerDistribution;
+	}
+	
+	public void laserOn(){
+		mMainActivity.tracking.mDetectorArry[mLazerDistribution].setLaser(true);
+	}
+	
+	public void laserOff(){
+		for(int i = 0; i < mNumOfTowers; i++){
+			mMainActivity.tracking.mDetectorArry[i].setLaser(false);
+		}
+	}
+	
+	public void laserWarningOn(){ 
+		for(int i = 0; i < mNumOfTowers; i++){
+			mMainActivity.tracking.mDetectorArry[i].setLaserWarning(true);
+		}
+	}
+	
+	public void laserWarningOff(){
+		for(int i = 0; i < mNumOfTowers; i++){
+			mMainActivity.tracking.mDetectorArry[i].setLaserWarning(false);
+		}
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*try {
+if (flag == true){
+	flag = false;
+	mTextDebug.setText(String.format("try"));
+}
+} catch (Exception e) {
+Log.d(TAG, "catch :" + e.getMessage());
+}*/
---
+++ sandbox/Bravo10.8_scaleable/src/com/example/android/opengl/MyGLRenderer.java
@@ -0,0 +1,376 @@
+/*
+ * Copyright (C) 2011 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.example.android.opengl;
+
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.nio.FloatBuffer;
+
+import java.sql.NClob;
+
+import javax.microedition.khronos.egl.EGLConfig;
+import javax.microedition.khronos.opengles.GL10;
+
+import org.opencv.samples.colorblobdetect.MarkerDetector;
+
+import com.example.bravo.MainActivity;
+
+import android.content.Context;
+import android.graphics.Point;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.opengl.GLSurfaceView;
+import android.opengl.GLU;
+import android.util.Log;
+import android.view.Display;
+import bravo.game.manager.GameManager;
+
+/**
+ * Provides drawing instructions for a GLSurfaceView object. This class
+ * must override the OpenGL ES drawing lifecycle methods:
+ * <ul>
+ *   <li>{@link android.opengl.GLSurfaceView.Renderer#onSurfaceCreated}</li>
+ *   <li>{@link android.opengl.GLSurfaceView.Renderer#onDrawFrame}</li>
+ *   <li>{@link android.opengl.GLSurfaceView.Renderer#onSurfaceChanged}</li>
+ * </ul>
+ */
+public class MyGLRenderer implements GLSurfaceView.Renderer {
+	private static final String  TAG              = "MyGLRenderer";
+	//-------------------------Static----------------------------//
+    private static final int     NO_COLOR = -1;
+	private static final int     BLUE_COLOR = 1;
+	private static final int     GREEN_COLOR = 2;
+	private static final int     RED_COLOR = 3;
+	
+	public final static int SS_SUNLIGHT = GL10.GL_LIGHT0;
+	//-------------------------Fields--------------------------//
+    private Triangle mTriangle;
+    private Square mSquare;
+    private example mExample;
+    private BlanderObj mBlanderObj;
+    private AssetObj mTower;
+    private AssetObj mCoin;
+    private AssetObj mTest;
+    private AssetObj mLaser;
+    private float mAngle;
+    
+    private float objX;
+    private float objY;
+    
+    private int screenWidth;
+    private int screenHeight;
+    
+    private  MarkerDetector[] detectorArray;
+    private  int num_of_towers;
+    private  MarkerDetector blueRedDetector;
+    private  MarkerDetector GreenRedDetector;
+    private  MarkerDetector GreenBlueDetector;
+    
+    SensorManager sensorManager;
+	int orientationSensor;
+	float headingAngle;
+	float pitchAngle;
+	float rollAngle;
+	
+	//-------------------------GameManger----------------------//
+	public GameManager mGameManager;
+    //-------------------------Activity------------------------//
+  	MainActivity                 mMainActivity;	
+  	private Context              appContext;
+  	//-------------------------Methods-------------------------//
+    
+  	public MyGLRenderer(Context context){
+  		appContext = context;
+  		
+  	}
+  	
+    @Override
+    public void onSurfaceCreated(GL10 gl, EGLConfig config) {
+        // Set the background frame color
+        gl.glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+        
+        load3dObjects();       
+        runOrientationSensor();
+        mGameManager = mMainActivity.mGameManager;
+        getScreenSizeInPixels(); // sets screenWidth and screenHeight
+        
+        //------Light settings--------------
+        initLighting(gl);
+        
+        //----------------------------------
+        objX = -0.5f; 
+        objY = 0.8f;
+        num_of_towers = mMainActivity.mGameManager.NUM_OF_TOWERS;
+        detectorArray = new MarkerDetector[num_of_towers];
+        for(int i = 0 ; i < num_of_towers ; i++){
+        	detectorArray[i] = new MarkerDetector();
+        	detectorArray[i].prepareGame(screenWidth, screenHeight, BLUE_COLOR, RED_COLOR, 4);
+        }
+        
+        int robpId = com.example.bravo.R.drawable.robo;
+        mGameManager.printBottomRight("screenWidth: " + screenWidth + " screenHeight: " + screenHeight);
+        mBlanderObj.createTexture(gl, appContext, robpId);
+        
+    }
+    
+    private void initLighting(GL10 gl) {
+    	float[] diffuse = {1.0f, 1.0f, 1.0f, 1.0f};
+    	float[] pos = {0.0f, 5.0f, 0.0f, 5.0f};
+    	gl.glLightfv(SS_SUNLIGHT, GL10.GL_POSITION, makeFloatBuffer(pos));
+    	gl.glLightfv(SS_SUNLIGHT, GL10.GL_DIFFUSE, makeFloatBuffer(diffuse));
+    	gl.glShadeModel(GL10.GL_SMOOTH);
+    	gl.glEnable(GL10.GL_LIGHTING);
+    	gl.glEnable(SS_SUNLIGHT);
+    	gl.glEnable(GL10.GL_COLOR_MATERIAL);
+    	float[] colorVector={1f, 1f, 1f, 1f};
+        gl.glLightModelfv(GL10.GL_LIGHT_MODEL_AMBIENT, makeFloatBuffer(colorVector));
+	}
+    
+    protected static FloatBuffer makeFloatBuffer(float[] arr) {
+    	ByteBuffer bb = ByteBuffer.allocateDirect(arr.length*4);
+    	bb.order(ByteOrder.nativeOrder());
+    	FloatBuffer fb = bb.asFloatBuffer();
+    	fb.put(arr);
+    	fb.position(0);
+    	return fb;
+    }
+    
+    private void load3dObjects(){
+    	mTriangle = new Triangle();
+        mSquare = new Square();
+        mExample = new example();
+        mBlanderObj = new BlanderObj();
+        mTower = new AssetObj(mMainActivity, "tower6.obj","tower6.mtl");//TODO
+        mCoin = new AssetObj(mMainActivity, "coin2.obj","coin2.mtl");
+        mTest = new AssetObj(mMainActivity, "tower2.obj","tower2.mtl");
+        mLaser = new AssetObj(mMainActivity, "lazer3.obj","lazer3.mtl");
+    }
+    
+    private void getScreenSizeInPixels(){
+    	Display display = mMainActivity.getWindowManager().getDefaultDisplay();
+        Point size = new Point();
+        display.getSize(size);
+        screenWidth = size.x;
+        screenHeight = size.y;
+    }
+
+    @Override
+    public void onDrawFrame(GL10 gl) {
+    	// Clears the screen and depth buffer.
+    	gl.glClearColor(0,0,0,0);
+    	gl.glClear(GL10.GL_COLOR_BUFFER_BIT | GL10.GL_DEPTH_BUFFER_BIT);
+    	// Replace the current matrix with the identity matrix
+    	gl.glViewport(0, 0, screenWidth, screenWidth);
+    	gl.glLoadIdentity();
+    	
+    	// SQUARE A
+    	for(int i = 0 ; i < num_of_towers ; i++){
+    		drawObj(gl, detectorArray[i]);
+        }
+    	drawLaser(gl);
+    	drawCoin(gl);
+    	
+    }
+
+	public void onSurfaceChanged(GL10 gl, int width, int height) {
+        // Adjust the viewport based on geometry changes
+        // such as screen rotations
+        gl.glViewport(0, 0, width, height);
+
+        // make adjustments for screen ratio
+        float ratio = (float) width / height;
+        gl.glMatrixMode(GL10.GL_PROJECTION);        // set matrix to projection mode
+        gl.glLoadIdentity();                        // reset the matrix to its default state
+        gl.glFrustumf(-ratio, ratio, -1, 1, 3, 7);  // apply the projection matrix
+    
+    }
+
+    /**
+     * Returns the rotation angle of the triangle shape (mTriangle).
+     *
+     * @return - A float representing the rotation angle.
+     */
+    public float getAngle() {
+        return mAngle;
+    }
+
+    /**
+     * Sets the rotation angle of the triangle shape (mTriangle).
+     */
+    public void setAngle(float angle) {
+        mAngle = angle;
+    }
+    
+    public void setScreenWidthHeight(int width, int height){
+    	screenWidth = width; 
+    	screenHeight = height;
+    }
+    
+    public void setCordinates(float x, float y){
+   	 	objX = x; 
+        objY = y;
+   }
+    
+    public float pixel2worldX(int x){
+    	return 2*((float)x) / screenWidth - 1;
+    }
+    
+    public float pixel2worldY(float y){
+    	// we want ower world to be scaled 1:1 so points 1 and -1 of the Y axis are out side the screen
+    	//that is why we use (screenHeight/ screenWidth) insted of 1
+    	float hightWidth = ((float)screenHeight / (float)screenWidth); // the (float) are importent.
+    	return y * hightWidth - (1f - hightWidth);  
+    }
+    
+    /*public float pixel2worldYscaled(int y){
+    	return 1 - 2*((float)y) / screenHeight;
+    }*/
+    
+    public float pixel2worldYscaled(int y){// need to add later
+    	float tYemp = 1 - 2*((float)y) / screenHeight;
+    	return pixel2worldY(tYemp);
+    }
+    
+    public float scaleTrackObjects(MarkerDetector mDetector){
+    	Log.i(TAG, "MyGLRenderer: mDetector xF - xB = " + (mDetector.getFrontX() - mDetector.getBackX()));
+    	Log.i(TAG, "MyGLRenderer: mDetector yF - yB = " + (mDetector.getFrontY() - mDetector.getBackY()));
+    	Log.i(TAG, "MyGLRenderer: mDetector.getFront2BackLength() = " + mDetector.getFront2BackLength());
+    	return 5*(mDetector.getFront2BackLength()) / mDetector.getFrameWidth();
+    }
+    
+    public void setTrackObjects(MarkerDetector[] mDetector){
+    	for(int i = 0 ; i < num_of_towers ; i++){
+    		detectorArray[i] = mDetector[i];
+        }
+    }
+    
+    public void setMainActivityContext(MainActivity activity){
+    	mMainActivity = activity;
+    }
+    
+    public void runOrientationSensor(){
+    	sensorManager = (SensorManager) mMainActivity.getSystemService(mMainActivity.SENSOR_SERVICE);
+		orientationSensor = Sensor.TYPE_ORIENTATION;
+		sensorManager.registerListener(sensorEventListener,sensorManager.getDefaultSensor(orientationSensor),SensorManager.SENSOR_DELAY_NORMAL);
+    }
+    
+    final SensorEventListener sensorEventListener = new SensorEventListener() {
+		public void onSensorChanged(SensorEvent sensorEvent) {
+			if (sensorEvent.sensor.getType() == Sensor.TYPE_ORIENTATION) {
+				headingAngle = sensorEvent.values[0];
+				pitchAngle = sensorEvent.values[1];
+				rollAngle = sensorEvent.values[2];
+				Log.d(TAG, "Heading: " + String.valueOf(headingAngle));
+				Log.d(TAG, "Pitch: " + String.valueOf(pitchAngle));
+				Log.d(TAG, "Roll: " + String.valueOf(rollAngle));
+			}
+		}
+
+		public void onAccuracyChanged(Sensor senor, int accuracy) {
+			// Not used
+		}
+	};
+    
+    public void drawObj(GL10 gl, MarkerDetector mDetector){
+		if (mDetector.isDetected() == false) {
+			return;
+		}
+		// Save the current matrix.
+		gl.glPushMatrix();
+		gl.glTranslatef(pixel2worldX(mDetector.getMiddleX()), pixel2worldYscaled(mDetector.getMiddleY()), 0);
+		float scale = 0.8f * scaleTrackObjects(mDetector);
+		gl.glScalef(scale, scale, scale); //(screenHeight/screenWidth)*		
+		float rotateOnZ = mDetector.getAngleFromXAxis();
+		gl.glRotatef(rotateOnZ, 0, 0, 1);
+		gl.glRotatef(rollAngle, (float)Math.cos(Math.toRadians(rotateOnZ)), -(float)Math.sin(Math.toRadians(rotateOnZ)), 0);
+		mTower.draw(gl);
+		// Restore the last matrix.
+		gl.glPopMatrix();
+	}
+    
+    public void drawCoin(GL10 gl){
+    	for(int i = 0 ; i < num_of_towers ; i++){
+    		if (detectorArray[i].hasCoin() == true){
+    			drawCoinToWorld(gl, detectorArray[i]);
+    		}
+        }
+    }
+    
+    private void drawLaser(GL10 gl) {
+    	for(int i = 0 ; i < num_of_towers ; i++){
+    		if (detectorArray[i].hasLaser() == true){
+    			drawLaserToWorld(gl, detectorArray[i]);
+    		}
+        }
+	}
+    
+    private void drawLaserToWorld(GL10 gl, MarkerDetector mDetector) {
+    	if (mDetector.isDetected() == false) {
+			return;
+		}
+		// Save the current matrix.
+		gl.glPushMatrix();
+		gl.glTranslatef(pixel2worldX(mDetector.getMiddleX()), pixel2worldYscaled(mDetector.getMiddleY()), 0);
+		float scale = 0.8f * scaleTrackObjects(mDetector);
+		gl.glScalef(scale, scale, scale); //(screenHeight/screenWidth)*		
+		float rotateOnZ = mDetector.getAngleFromXAxis();
+		gl.glRotatef(rotateOnZ, 0, 0, 1);
+		gl.glRotatef(rollAngle, (float)Math.cos(Math.toRadians(rotateOnZ)), -(float)Math.sin(Math.toRadians(rotateOnZ)), 0);
+		mLaser.draw(gl);
+		// Restore the last matrix.
+		gl.glPopMatrix();		
+	}
+
+	public void drawCoinToWorld(GL10 gl, MarkerDetector mDetector){
+    	gl.glPushMatrix();
+		gl.glTranslatef(pixel2worldX(mDetector.getCoinX()), pixel2worldYscaled(mDetector.getCoinY()), 0);
+		float scale = 0.2f * scaleTrackObjects(mDetector);
+		gl.glScalef(scale, scale, scale); 	
+		float rotateOnZ = mDetector.getCoinAngleInDegAroundItSelf() + mDetector.getAngleFromXAxis(); //TODO rotate according to time;
+		gl.glRotatef(rotateOnZ, 0, 0, 1);
+		gl.glRotatef(rollAngle, (float)Math.cos(Math.toRadians(rotateOnZ)), -(float)Math.sin(Math.toRadians(rotateOnZ)), 0);
+				
+		// Draw mExample A.
+		mCoin.draw(gl);
+		// Restore the last matrix.
+		gl.glPopMatrix();
+    }
+    
+    public void drawGrid(GL10 gl,float x, float y, boolean flag){
+		// Save the current matrix.
+		gl.glPushMatrix();
+		//gl.glTranslatef(pixel2worldX(mDetector.getMiddleX()), pixel2worldY(mDetector.getMiddleY()), 0);
+		gl.glTranslatef(x, pixel2worldY(y), 0);
+		
+		float scale = 0.5f;
+		Log.i(TAG, "MyGLRenderer: sacle = " + scale);
+		gl.glScalef(scale, scale, scale); 
+		//gl.glRotatef(45,0,0,1);				
+		// Draw mExample A.
+		//mExample.draw(gl);
+		if (flag){
+			mTriangle.draw(gl);
+		}
+		mTower.draw(gl);
+		// Restore the last matrix.
+		gl.glPopMatrix();
+	}
+    
+    
+    
+}
