4514fd9e0f52e3b1aa2692811571d695eae64386
alonamar <alonamar2@gmail.com>
2016-07-28 15:33:57 +0300

    Bravo10.8_scaleable_Towers

--- sandbox/Bravo10.8a_scaleable & changeable/src/com/MobileAnarchy/Android/Widgets/Joystick/JoystickMovedListener.java
+++
@@ -1,7 +0,0 @@
-package com.MobileAnarchy.Android.Widgets.Joystick;
-
-public interface JoystickMovedListener {
-	public void OnMoved(double pan, double tilt);
-	public void OnReleased();
-	public void OnReturnedToCenter();
-}
